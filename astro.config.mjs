06d47f115269e4b30db1093517235f116f289c84
Alf Lionel <apwcow@proton.me>
2026-06-04 22:25:55 +0100

    stylish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://alflionel.github.io",
   base: "/draw-a-perfect-star",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
